ff3d8b6481927f45edc5fd7066dabf54a7d0454c
staketd <pushind00@yandex.ru>
2019-04-28 22:32:59 +0300

    port fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,8 @@
 import telebot
 from collections import defaultdict
+from config import TOKEN
 
-bot = None
 
 EVENTS = defaultdict(dict)
 WEATHER_API_KEY = '6a1eaa0e205d9707ac2606ac7e191a47'
-
-
-def init_bot(token):
-    global bot
-    bot = telebot.TeleBot(token)
-    from app import handlers
+bot = telebot.TeleBot(TOKEN)
--- run.py
+++ run.py
@@ -1,11 +1,25 @@
-import app
-from TOKEN import TOKEN
+import flask
+import os
+from telebot import types
+from config import TOKEN
+from app import bot
+
+server = flask.Flask(__name__)
 
 
-def main():
-    app.init_bot(TOKEN)
-    app.bot.polling()
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates(
+        [types.Update.de_json(flask.request.stream.read().decode('utf-8'))])
+    return '!', 200
+
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://python-review-2.herokuapp.com/' + TOKEN)
+    return '!', 200
 
 
 if __name__ == '__main__':
-    main()
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
