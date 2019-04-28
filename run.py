0e4c9987e7dd9c4bccf523e4ebf0ccc6906e67ad
staketd <pushind00@yandex.ru>
2019-04-28 22:47:12 +0300

    port fix

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def get_message():
     return '!', 200
 
 
-@server.route('/', methods=['GET'])
+@server.route('/')
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url='https://python-review-2.herokuapp.com/' + TOKEN)
