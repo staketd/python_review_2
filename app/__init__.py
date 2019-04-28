4d1712238845e545308702f0c1533895ca6129a7
staketd <pushind00@yandex.ru>
2019-04-28 22:49:52 +0300

    port fix

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,4 @@ from config import TOKEN
 
 EVENTS = defaultdict(dict)
 WEATHER_API_KEY = '6a1eaa0e205d9707ac2606ac7e191a47'
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN, threaded=True)
